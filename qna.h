3bc2b04f1a5965b15797623e11e416b5ed97e2ec
Ivan <107774921+gl0v4nnl@users.noreply.github.com>
2023-06-30 13:27:19 +0300

    Update qna.h

--- qna.h
+++ qna.h
@@ -8,12 +8,13 @@ class qna {
     std::map<std::string, std::string> qmap = {
         {"exit", "bye"},
         {"start", "let's go"},
+        {"hello", "hi"},
     };
     int count = 0;  
-    int last_number = 1;  
+    const int last_number = 3;  
 public:
     bool add(std::string new_question, std::string new_answer);
-    bool check(std::string check_string);
+    bool reply(std::string check_string);
     bool remove(std::string rem_question);
 };
 
